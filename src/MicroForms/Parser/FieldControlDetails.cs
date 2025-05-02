236c7af3b3aa16ead443a1c20cdfc5d3119e1ae3
Mimino74 <mimino.arah@gmail.com>
2025-05-03 06:38:07 +0700

    added form field details model

---
+++ src/MicroForms/Parser/FieldControlDetails.cs
@@ -0,0 +1,24 @@
+using MicroForms.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MicroForms;
+
+public class FieldControlDetails: ContainerDetails
+{
+    //public string ModelBinding { get; set; }
+    //public string ModelBindingType { get; set; }
+    //public string ModelItems { get; set; }
+    //public string ModelItemId { get; set; }
+    //public string ModelItemName { get; set; }
+    //public string ModelTableBinding { get; set; }
+    //public string ModelTargetBinding { get; set; }
+
+    // new binding concept
+    public FieldBinding Binding { get; set; }
+
+    // Navigation
+    public string ActionLink { get; set; }
+}
